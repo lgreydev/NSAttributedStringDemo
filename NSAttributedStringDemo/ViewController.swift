bbf6200634952fa88f3c4ea76d2f8c53f1db6039
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-05-08 17:21:03 +0300

    Add TextView

--- NSAttributedStringDemo/ViewController.swift
+++ NSAttributedStringDemo/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import TinyConstraints
 
 class ViewController: UIViewController {
+
+    lazy var textView: UITextView = {
+        let view = UITextView()
+        view.backgroundColor = .yellow
+        view.text = "A string with associated attributes (such as visual style, hyperlinks, or accessibility data) for portions of its text."
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+
+        addViews()
+        addConstrains()
     }
 
 
 }
 
+
+extension ViewController{
+    private func addViews() {
+        view.addSubview(textView)
+    }
+
+    private func addConstrains() {
+        constrainToTextView()
+    }
+
+    private func constrainToTextView() {
+        textView.height(200)
+        textView.centerInSuperview()
+        textView.leadingToSuperview()
+        textView.trailingToSuperview()
+    }
+}
